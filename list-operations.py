7d96137909fc546683eb8e12558c89ba82a79da7
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-01-08 11:46:11 +0200

    Create list-operations.py

---
+++ list-operations.py
@@ -0,0 +1,6 @@
+commands = ["Lights Off", "Lock the door", "Open the door", "Make Coffee", "Shut down"]
+command = input()
+if command in commands:
+    print("OK")
+else:
+    print("Not supported")
