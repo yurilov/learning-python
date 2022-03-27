902d37f14b6c1eedcac6bc828602f6b3f51b74fb
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-03-27 11:16:18 +0300

    added color categorizer and comments

--- progs/elif-statements.py
+++ progs/elif-statements.py
@@ -1,3 +1,5 @@
+# age checker
+
 age = int(input())
 name = input()
 
@@ -6,11 +8,12 @@ if age >= 18:
 else:
     print('Sorry')
 
+# right-angled triangle checker
 
 side1 = int(input())
 side2 = int(input())
 side3 = int(input())
-# your code goes here
+
 sum_of_squares = side1 * side1 + side2 * side2
 square_of_hypotenuse = side3 * side3
 
@@ -18,3 +21,14 @@ if sum_of_squares == square_of_hypotenuse:
     print('Right-angled')
 else:
     print("Not right-angled")
+
+# color categorizer
+
+color = input()
+
+if color == 'red':
+    print(1)
+elif color == 'green':
+    print(2)
+elif color == 'black':
+    print(3)
