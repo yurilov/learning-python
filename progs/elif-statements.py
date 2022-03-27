52e0c394af15d490e310f970b8323d7e51f97c0c
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-03-27 11:12:30 +0300

    now we can check if the triangle is right angled

--- progs/elif-statements.py
+++ progs/elif-statements.py
@@ -5,3 +5,16 @@ if age >= 18:
     print("Welcome " + name)
 else:
     print('Sorry')
+
+
+side1 = int(input())
+side2 = int(input())
+side3 = int(input())
+# your code goes here
+sum_of_squares = side1 * side1 + side2 * side2
+square_of_hypotenuse = side3 * side3
+
+if sum_of_squares == square_of_hypotenuse:
+    print('Right-angled')
+else:
+    print("Not right-angled")
