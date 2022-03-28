68b9ed38b970f7d4c8bf51c46fbe4f3724a7ca17
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-03-28 08:58:14 +0300

    added age groups checker

--- progs/booleans.py
+++ progs/booleans.py
@@ -12,3 +12,15 @@ if day >= 1 and day < 6 and hour >= 10 and hour <= 21:
     print('Open')
 else:
     print('Closed')
+
+# age groups
+age = int(input())
+
+if age >= 0 and age <= 11:
+    print('Child')
+elif age >= 12 and age <= 17:
+    print('Teen')
+elif age >= 18 and age <= 64:
+    print('Adult')
+else:
+    print('Senior')
