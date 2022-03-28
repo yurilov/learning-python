9b97b15a29c330ec9d0d214ba76fb98c451676dc
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-03-28 08:54:46 +0300

    checking if the store is open

--- progs/booleans.py
+++ progs/booleans.py
@@ -1,3 +1,14 @@
+# humidity checker
+
 humidity = int(input())
 if humidity >= 40 and humidity <= 60:
     print('norm')
+
+# is store open?
+hour = int(input())
+day = int(input())
+
+if day >= 1 and day < 6 and hour >= 10 and hour <= 21:
+    print('Open')
+else:
+    print('Closed')
