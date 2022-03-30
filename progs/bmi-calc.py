a81f54798492de4b8a757cbd5ae3d37d70e05152
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-03-30 09:34:50 +0300

    added discount level identififer

--- progs/bmi-calc.py
+++ progs/bmi-calc.py
@@ -16,3 +16,18 @@ else:
 type = input()
 if type == 'Visa' or type == 'Amex':
     print('accepted')
+
+# discount level
+
+score1 = int(input())
+score2 = int(input())
+average = (score1 + score2) / 2
+
+if average <= 100 and average >= 90:
+    print(50)
+elif average <= 89 and average >= 80:
+    print(30)
+elif average <= 79 and average >= 70:
+    print(10)
+else:
+    print(0)
