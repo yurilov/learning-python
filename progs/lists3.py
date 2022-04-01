48e67196e68a84ea981fdf04c05f55add0cf0d1c
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-04-01 10:18:57 +0300

    sum of min and max values from the numbers list

--- progs/lists3.py
+++ progs/lists3.py
@@ -13,3 +13,14 @@ items = [2, 4, 6, 8, 10, 12, 14]
 length = len(items)
 middle_index = length // 2
 print(middle_index)
+
+# sum of min and max numbers
+
+numbers = [14, 5, 6, 8, 17, 28]
+
+min_value = min(numbers)
+max_value = max(numbers)
+
+sum = min_value + max_value
+
+print(sum)
