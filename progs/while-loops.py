922c8c3a784b8630c2b894e7c50fb7f436b9d061
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-04-02 12:08:22 +0300

    calculating cart sum with discount

--- progs/while-loops.py
+++ progs/while-loops.py
@@ -43,3 +43,22 @@ while True:
     items.append(n)
 
 print(items)
+
+# get item for free if price is odd
+
+items = [23, 555, 666, 123, 128, 4242, 990]
+
+sum = 0
+n = 0
+
+while n < len(items):
+
+    num = items[n]
+    n += 1
+
+    if num % 2 == 1:
+        continue
+
+    sum += num
+
+print(sum)
