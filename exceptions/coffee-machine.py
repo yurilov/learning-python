ada0016acc21a15121441d72fdf9d51e6466f9ec
Yurii Shchepilov <89077590+yurilov@users.noreply.github.com>
2022-04-25 11:11:03 +0300

    added coffee machine.py

---
+++ exceptions/coffee-machine.py
@@ -0,0 +1,11 @@
+coffee = ["Café Latte", "Caffe Americano",
+          "Espresso", "Cappuccino", "Macchiato"]
+
+choice = int(input())
+
+try:
+    print(coffee[choice])
+except:
+    print('Invalid number')
+finally:
+    print('Have a good day')
